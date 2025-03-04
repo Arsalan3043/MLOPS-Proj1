780050262e2154cb74ebc808100f491d82ae697d
Arsalan3043 <arsalankhanrkl@gmail.com>
2025-03-04 23:51:18 +0400

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arsalan",
+    author_email="arsalankhanrkl@gmail.com",
+    packages=find_packages()
+)
